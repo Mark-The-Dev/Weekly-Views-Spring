020ab7b5f42aa5681256aad1861581b9dd71b10c
Mark <SwiftTactics@gmail.com>
2021-04-30 04:34:27 -0400

    added SQL create string

--- src/main/java/com/DevMark/Weeks_View/repositories/UserRepositoryImpl.java
+++ src/main/java/com/DevMark/Weeks_View/repositories/UserRepositoryImpl.java
@@ -2,8 +2,14 @@ package com.DevMark.Weeks_View.repositories;
 
 import com.DevMark.Weeks_View.domain.User;
 import com.DevMark.Weeks_View.exceptions.WvAuthException;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UserRepositoryImpl implements UserRepository {
+
+    private static final String SQL_CREATE = "INSERT INTO WV_USERS(USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD)" +
+            "VALUES(NEXTVAL('WV_USERS-SEQ'), ?, ?, ?, ?)";
+
     @Override
     public Integer create(String firstName, String lastName, String email, String password) throws WvAuthException {
         return null;
