3f1d10ed36e0b15259510e0260c7c57b7edffaf1
Mark <SwiftTactics@gmail.com>
2021-05-03 20:17:49 -0400

    added user register controller

--- src/main/java/com/DevMark/Weeks_View/resources/UserResource.java
+++ src/main/java/com/DevMark/Weeks_View/resources/UserResource.java
@@ -1,17 +1,27 @@
 package com.DevMark.Weeks_View.resources;
 
+import com.DevMark.Weeks_View.domain.User;
+import com.DevMark.Weeks_View.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserResource {
 
-    // simple post test
+    @Autowired
+    UserService userService;
+
+
+     //simple post test
 //    @PostMapping("/register")
 //    public String registerUser(@RequestBody Map<String, Object> userMap){
 //        String firstName = (String) userMap.get("first_name");
@@ -21,6 +31,22 @@ public class UserResource {
 //
 //        return firstName + ", " + lastName + ", " + email +", " + password;
 //    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Map<String, String >> registerUser(@RequestBody Map<String, Object> userMap){
+        String firstName = (String) userMap.get("firstName");
+        String lastName = (String) userMap.get("lastName");
+        String email = (String) userMap.get("email");
+        String password = (String) userMap.get("password");
+
+        User user = userService.registerUser(firstName, lastName, email, password);
+        Map<String, String> map = new HashMap<>();
+        // eventually will put jwt here
+        map.put("message", "registered successfully");
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+
+    }
 
 
 
